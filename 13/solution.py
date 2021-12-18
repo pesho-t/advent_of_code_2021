bdf60bb6a0cfadd76cebea4467e322846168fda4
Petar T <info@petar.t>
2021-12-18 15:20:14 +0000

    Day 13 p 2

--- 13/solution.py
+++ 13/solution.py
@@ -1,34 +1,12 @@
 #!/usr/bin/env python3
 
-from typing import List, NamedTuple, Tuple
+from copy import deepcopy
+from typing import List, NamedTuple
+
 
 class Fold(NamedTuple):
     axis: str
     coord: int
-
-
-def parse_coords(coords: List[str]) -> List[List[int]]:
-    grid: List[List[int]] = [[0]]
-    width = 1
-    height = 1
-
-    for pair in coords:
-        x_str, y_str = pair.strip("\n").split(",")
-        x = int(x_str)
-        y = int(y_str)
-
-        if y + 1 > height:
-            for col in grid:
-                col.extend([0 for _ in range(y + 1 - height)])
-            height = y + 1
-
-        if x + 1 > width:
-            grid.extend([[0 for _ in range(height)] for _ in range(x + 1 - width)])
-            width = x + 1
-
-        grid[x][y] = 1
-
-    return grid
 
 
 def parse_folds(lines: List[str]) -> List[Fold]:
@@ -41,33 +19,16 @@ def parse_folds(lines: List[str]) -> List[Fold]:
     return folds
 
 
-def slice_grid(grid: List[List[int]], axis: str, coord: int) -> Tuple[List[List[int]], List[List[int]]]:
-    if axis == "x":
-        top: List[List[int]] = list()
-        bottom: List[List[int]] = list()
-
-        for col in grid:
-            top.append(col[:coord])
-            bottom.append(col[coord+1:])
-            return top, bottom
-    elif axis == "y":
-        left = grid[:coord]
-        right = grid[coord+1:]
-        return left, right
-    else:
-        raise ValueError(f"Invalid axis {axis}")
-
-
 def fold_grid(grid: List[List[int]], axis: str, coord: int) -> List[List[int]]:
     folded: List[List[int]] = list()
-    grid_copy = grid.copy()
+    grid_copy = deepcopy(grid)
 
     if axis == "x":
         dest_start = 0
-        src_start = len(grid) - 1
+        src_start = len(grid_copy) - 1
         while dest_start < coord < src_start:
-            for y in range(len(grid[0])):
-                grid_copy[dest_start][y] = 1 if grid[dest_start][y] or grid[src_start][y] else 0
+            for y in range(len(grid_copy[0])):
+                grid_copy[dest_start][y] = 1 if grid_copy[dest_start][y] or grid_copy[src_start][y] else 0
             dest_start += 1
             src_start -= 1
 
@@ -75,8 +36,29 @@ def fold_grid(grid: List[List[int]], axis: str, coord: int) -> List[List[int]]:
             folded.append(grid_copy[x])
 
         return folded
+    elif axis == "y":
+        dest_start = 0
+        src_start = len(grid_copy[0]) - 1
+        while dest_start < coord < src_start:
+            for x in range(len(grid_copy)):
+                grid_copy[x][dest_start] = 1 if grid_copy[x][dest_start] or grid_copy[x][src_start] else 0
+            dest_start += 1
+            src_start -= 1
+
+        for col in grid_copy:
+            folded.append(col[:coord])
+
+        return folded
     else:
         ValueError("Wrong folding axes")
+
+
+def print_grid(grid: List[List[int]]) -> None:
+    for y in range(len(grid[0])):
+        row = ""
+        for x in range(len(grid)):
+            row += "#" if grid[x][y] else " "
+        print(row)
 
 
 def main():
@@ -91,18 +73,40 @@ def main():
                 break
             coords.append(coord)
 
-        grid = parse_coords(coords)
-
         folds = parse_folds(lines)
 
+        max_x = 0
+        max_y = 0
+        for fold in folds:
+            if fold.axis == "x":
+                max_x = max(max_x, fold.coord * 2 + 1)
+            elif fold.axis == "y":
+                max_y = max(max_y, fold.coord * 2 + 1)
+            else:
+                ValueError(f"Invalid axis: {fold.axis}")
+
+        grid = [[0 for _ in range(max_y)] for _ in range(max_x)]
+
+        for coord in coords:
+            x, y = coord.rstrip("\n").split(",")
+            grid[int(x)][int(y)] = 1
+
         # Part 1
-        folded = fold_grid(grid, folds[0].axis, folds[0].coord)
+        fold = folds.pop(0)
+        folded = fold_grid(grid, fold.axis, fold.coord)
+
         grid_sum = 0
         for col in folded:
             for cell in col:
                 grid_sum += cell
 
-        print(grid_sum)
+        print(f"P1: {grid_sum}")
+
+        # Part 2
+        for fold in folds:
+            folded = fold_grid(folded, fold.axis, fold.coord)
+
+        print_grid(folded)
 
 
 if __name__ == "__main__":
